22d1a3d595cae6fa632d4f0b29f7a57b2d5144f5
khizar-s <59326221+khizar-s@users.noreply.github.com>
2020-04-25 20:26:14 -0400

    Initializing the types of dots to play with

---
+++ 2 Dots/src/DotT.java
@@ -0,0 +1,11 @@
+/**
+ * @author Khizar Siddiqui
+ * Description: Enums for Dot colors
+ */
+
+/**
+ * @brief Colors to use for dots
+ */
+public enum DotT {
+    RED, GREEN, BLUE, ORANGE, PINK, EMPTY;
+}
